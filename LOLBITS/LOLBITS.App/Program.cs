2e64cdeabd6e29c987f6d0285b112fc0bed116a8
Diego Miguel Yusta <diego.yusta@soderbergpartners.se>
2020-01-07 17:35:14 +0100

    Renamed the startup project. Added .gitignore file. Removed files that shouldn't be under version control.

---
+++ LOLBITS/LOLBITS.App/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+namespace LOLBITS
+{
+    public class Program
+    {
+
+        private const string FirstId = "abcde1234";
+        private const string Url = "http://192.168.1.69/final/";
+        private const string Password = "password";
+ 
+
+        static void Main(string[] args)
+        {
+
+            Controler c = new Controler(FirstId, Url, Password);
+            c.Start();
+
+        }
+
+    }
+
+}
